46bc44215beb0e25dfe7d6314709527f6fadb6e9
dennyzhang <denny.zhang001@gmail.com>
2016-04-10 10:16:33 +0800

    update variable default value

--- bash/enable_common_library.sh
+++ bash/enable_common_library.sh
@@ -7,7 +7,7 @@
 ##               By default, this file keeps stable and untouched
 ## --
 ## Created : <2016-04-07>
-## Updated: Time-stamp: <2016-04-08 06:25:22>
+## Updated: Time-stamp: <2016-04-10 10:15:42>
 ##-------------------------------------------------------------------
 function refresh_common_library() {
     local library_file=${1?}
@@ -39,7 +39,7 @@ function enable_common_library() {
 
 # When checksum is not given, we will force re-download
 file_checksum=${1:-"checksum for common bash library"}
-library_download_path=${2:-"/tmp/bash_common_library.sh"}
+library_download_path=${2:-"/var/lib/bash_common_library.sh"}
 library_url=${3:-"https://raw.githubusercontent.com/DennyZhang/devops_public/master/bash/bash_common_library.sh"}
 
 enable_common_library $library_download_path $library_url $file_checksum
