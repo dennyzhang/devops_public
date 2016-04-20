e691983514f9a93c41b2813c498fa84fbf84271e
Jenkins Auto <jenkins.auto@dennyzhang.com>
2016-04-20 21:18:06 +0800

    code refine

--- chef/enable_chef_depoyment.sh
+++ chef/enable_chef_depoyment.sh
@@ -9,18 +9,21 @@
 ## Description :
 ## --
 ## Created : <2016-04-20>
-## Updated: Time-stamp: <2016-04-20 21:01:30>
+## Updated: Time-stamp: <2016-04-20 21:12:33>
 ##-------------------------------------------------------------------
 ################################################################
 # How To Use
 #        export git_update_url="https://raw.githubusercontent.com/TOTVS/mdmpublic/master/git_update.sh"
 #        export ssh_email="denny.devops@dennyzhang.com"
+#
 #        export ssh_public_key="AAAAB3NzaC1yc2EAAAADAQABAAABAQClL5PmH01x8eRPQ7FsodNT172ZIXiE2CT3RhBZpPpMFCdUyFTGBRfgbX/UE86MfycPHkzNnKemFNJOqFVdzK7eTIayxX9FYPOk+ONi2sbKkwAE4No+R0d4/ehoVzflbYXRWyxLqDKkqbJPDxY39xS2V7h4bSQWwrMyeYoGBn82AW5vSoonQMIrxe+bm6zWWtL6SzsYM/KNM1T+2pfU7Rq/YQPs2tf07rauyeT3bylhUf/CUqVPt2Xpf4qgmpGqp9Hyoy7FIfBHmCgXLRpia2KhpYr0j08s8cxBx1PEJiQ6EaWO2WlzyJIqgU2t9piDHEIUd6yCPmpshLtlOvno6KN5"
+#
 #        export ssh_config_content="Host github.com
 #          StrictHostKeyChecking no
 #          User git
 #          HostName github.com
 #          IdentityFile /root/.ssh/git_id_rsa"
+#
 #        export git_deploy_key="-----BEGIN RSA PRIVATE KEY-----
 #        MIIJKgIBAAKCAgEAq6Jv5VPd82Lu2WE3R4/lNeA5Txckf3FE3aKRVBhRWy1ds1V9
 #        ... ...
@@ -47,6 +50,7 @@ function enable_chef_deployment() {
     local git_deploy_key=${4?}
     local ssh_config_content=${5?}
 
+    log "enable chef deployment"
     install_packages "wget" "wget"
     install_packages "git" "git"
     download_facility $git_update_url "/root/git_update.sh"
@@ -66,18 +70,35 @@ function install_packages() {
 function download_facility() {
     local url=${1?}
     local dst_file=${2:?}
-    wget -O $dst_file $url
-    chmod 755 $dst_file
+    if [ ! -f $dst_file ]; then
+        command="wget -O $dst_file $url"
+        log "$command"
+        $command
+        chmod 755 $dst_file
+    fi
 }
 
 function inject_git_deploy_key() {
     local ssh_key=${1?}
     shift
     local ssh_key_content=$*
+
+    log "inject git deploy key to $ssh_key"
     cat > $ssh_key <<EOF
 $ssh_key_content
 EOF
     chmod 400 $ssh_key
+}
+
+function git_ssh_config() {
+    local ssh_config_file=${1?}
+    shift
+    local ssh_config_content=$*
+
+    log "configure $ssh_config_file"
+    cat > $ssh_config_file <<EOF
+$ssh_content
+EOF
 }
 
 function inject_ssh_authorized_keys() {
@@ -85,18 +106,11 @@ function inject_ssh_authorized_keys() {
     local ssh_public_key=${2?}
 
     local ssh_authorized_key_file="/root/.ssh/authorized_keys"
-    if ! grep $email $ssh_authorized_key_file 2>&1 1>/dev/null; then
+
+    log "inject ssh authorized keys to $ssh_authorized_key_file"
+    if ! grep $ssh_email $ssh_authorized_key_file 2>&1 1>/dev/null; then
         echo "ssh-rsa $ssh_public_key $ssh_email" >> $ssh_authorized_key_file
     fi
-}
-
-function git_ssh_config() {
-    local ssh_config_file=${1?}
-    shift
-    local ssh_config_content=$*
-    cat > $ssh_config_file <<EOF
-$ssh_content
-EOF
 }
 ####################################
 enable_chef_deployment "$git_update_url" "$ssh_email" "$ssh_public_key" "$git_deploy_key" "$ssh_config_content"
