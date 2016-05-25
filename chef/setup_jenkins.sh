9ec718ef16bd3581e59f2196962feecc7f86063d
dennyzhang <denny@dennyzhang.com>
2016-05-25 17:33:33 +0800

    grant jenknis root privelege

--- chef/setup_jenkins.sh
+++ chef/setup_jenkins.sh
@@ -9,7 +9,7 @@
 ## Description :
 ## --
 ## Created : <2016-04-20>
-## Updated: Time-stamp: <2016-05-25 17:20:36>
+## Updated: Time-stamp: <2016-05-25 17:33:14>
 ##-------------------------------------------------------------------
 function install_jenkins() {
     if ! (dpkg -s jenkins | grep "Status: install" 1>/dev/null 2>&1); then
@@ -18,8 +18,7 @@ function install_jenkins() {
         sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
         apt-get update
         apt-get install -y jenkins
-        # Grant privilege to jenkins user
-        echo '%jenkins ALL=(ALL:ALL) NOPASSWD: ALL' > /etc/sudoers.d/jenkins
+
     fi
 }
 
@@ -43,8 +42,15 @@ function setup_jenkins_jobs() {
     fi
 }
 
-install_jenkins
+function grant_jenkins_privilege() {
+    if [ ! -f /etc/sudoers.d/jenkins ]; then
+        echo '%jenkins ALL=(ALL:ALL) NOPASSWD: ALL' > /etc/sudoers.d/jenkins
+    fi
+}
 
+
+grant_jenkins_privilege
+install_jenkins
 setup_jenkins_jobs
 
 echo "Jenkins is up: http://\$server_ip:8080"
