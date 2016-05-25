31339adb6093e83992416538397a3b79e9be622f
dennyzhang <denny@dennyzhang.com>
2016-05-25 16:28:58 +0800

    add jenkins scripts

---
+++ chef/setup_jenkins.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+##-------------------------------------------------------------------
+## @copyright 2016 DennyZhang.com
+## Licensed under MIT
+##   https://raw.githubusercontent.com/DennyZhang/devops_public/master/LICENSE
+##
+## File : setup_jenkins.sh
+## Author : Denny <denny@dennyzhang.com>
+## Description :
+## --
+## Created : <2016-04-20>
+## Updated: Time-stamp: <2016-05-25 16:28:08>
+##-------------------------------------------------------------------
+# TODO: setup jenkins
+
+# TODO: configure jenkins jobs
+
+# TODO: restart jenkins
+
+## File : setup_jenkins.sh ends
