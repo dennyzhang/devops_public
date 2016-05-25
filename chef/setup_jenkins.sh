c23702d0ad34c5eca2081dc9d92630f3e7ef6be6
dennyzhang <denny@dennyzhang.com>
2016-05-25 17:01:48 +0800

    implement jenkins setup logic

--- chef/setup_jenkins.sh
+++ chef/setup_jenkins.sh
@@ -2,19 +2,48 @@
 ##-------------------------------------------------------------------
 ## @copyright 2016 DennyZhang.com
 ## Licensed under MIT
-##   https://raw.githubusercontent.com/DennyZhang/devops_public/master/LICENSE
+## https://raw.githubusercontent.com/DennyZhang/devops_public/master/LICENSE
 ##
 ## File : setup_jenkins.sh
 ## Author : Denny <denny@dennyzhang.com>
 ## Description :
 ## --
 ## Created : <2016-04-20>
-## Updated: Time-stamp: <2016-05-25 16:28:08>
+## Updated: Time-stamp: <2016-05-25 16:57:13>
 ##-------------------------------------------------------------------
-# TODO: setup jenkins
+function install_jenkins() {
+    if ! (dpkg -s jenkins | grep "Status: install" 1>/dev/null 2>&1); then
+        echo "setup jenkins"
+        wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
+        sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+        apt-get update
+        apt-get install -y jenkins
+    fi
+}
 
-# TODO: configure jenkins jobs
+function setup_jenkins_jobs() {
+    jenkins_jobs="DeploySystem,https://raw.githubusercontent.com/TOTVS/mdmpublic/master/chef/jenkins_jobs/deploysystem_config.xml"
+    should_restart=false
+    for job in $jenkins_jobs; do
+        item=(${job//,/ })
+        job_name=${item[0]}
+        job_url=${item[1]}
+        mkdir -p "/var/lib/jenkins/jobs/$job_name"
+        echo "Download config.xml for Jenkins job $job_name"
+        curl -o "/var/lib/jenkins/jobs/$job_name/config.xml" "$job_url"
+        should_restart=true
+    done
 
-# TODO: restart jenkins
+    if $should_restart; then
+        chown jenkins:jenkins -R /var/lib/jenkins/jobs
+        echo "reload Jenkins jobs"
+        service jenkins force-reload
+    fi
+}
 
+install_jenkins
+
+setup_jenkins_jobs
+
+echo "Jenkins is up: http://\$server_ip:8080"
 ## File : setup_jenkins.sh ends
