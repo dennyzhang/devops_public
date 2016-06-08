7fc96a9b3478fd5486dd998c8be443bd65e4ebe0
dennyzhang <denny@dennyzhang.com>
2016-06-08 14:19:48 +0800

    remove useless function

--- chef/devops_provision_os.sh
+++ chef/devops_provision_os.sh
@@ -9,7 +9,7 @@
 ## Description :
 ## --
 ## Created : <2016-04-20>
-## Updated: Time-stamp: <2016-06-08 14:01:37>
+## Updated: Time-stamp: <2016-06-08 14:18:58>
 ##-------------------------------------------------------------------
 . /etc/profile
 
@@ -22,42 +22,8 @@ fi
 bash /var/lib/devops/refresh_common_library.sh
 . /var/lib/devops/devops_common_library.sh
 ################################################################################
-function inject_git_deploy_key() {
-    local ssh_key=${1?}
-    shift
-    local ssh_key_content=$*
-
-    log "inject git deploy key to $ssh_key"
-    cat > "$ssh_key" <<EOF
-$ssh_key_content
-EOF
-    chmod 400 "$ssh_key"
-}
-
-function git_ssh_config() {
-    local ssh_config_file=${1?}
-    shift
-    local ssh_config_content="$*"
-
-    log "configure $ssh_config_file"
-    cat > "$ssh_config_file" <<EOF
-$ssh_config_content
-EOF
-}
-
-################################################################
-if [ -z "$ssh_email" ]; then
-    export ssh_email="auto.devops@totvs.com"
-fi
-
-if [ -z "$ssh_config_content" ]; then
-    export ssh_config_content="Host github.com
-  StrictHostKeyChecking no
-  User git
-  HostName github.com
-  IdentityFile /root/.ssh/git_id_rsa"
-fi
-
+chef_version="12.4.1"
+ssh_email="auto.devops@totvs.com"
 ssh_public_key_file="/root/ssh_id_rsa.pub"
 git_deploy_key_file="/root/git_deploy_key"
 
@@ -70,11 +36,10 @@ if [ -z "$git_deploy_key" ] && [ -f "$git_deploy_key_file" ]; then
     export git_deploy_key
     git_deploy_key=$(cat "$git_deploy_key_file")
 fi
-
-################################################################
+################################################################################
 log "enable chef deployment"
 install_package_list "wget,curl,git"
-install_chef "12.4.1"
+install_chef $chef_version
 
 download_facility "/root/git_update.sh" \
                   "https://github.com/DennyZhang/devops_public/raw/master/bash/git_update.sh"
@@ -89,11 +54,18 @@ fi
 
 # support git clone for DevOps code
 if [ -n "$git_deploy_key" ]; then
-    inject_git_deploy_key "/root/.ssh/git_id_rsa" "$git_deploy_key"
-fi
-
-if [ -n "$ssh_config_content" ]; then
-    git_ssh_config "/root/.ssh/config" "$ssh_config_content"
+    git_key_file="/root/.ssh/git_id_rsa"
+    cat > "$git_key_file" <<EOF
+$git_deploy_key
+EOF
+    chmod 400 git_key_file
+    cat > "/root/.ssh/config" <<EOF
+Host github.com
+  StrictHostKeyChecking no
+  User git
+  HostName github.com
+  IdentityFile $git_key_file
+EOF
 fi
 
 echo "Action Done"
