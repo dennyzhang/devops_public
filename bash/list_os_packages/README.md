2f9c0361861e467356b90eebc596072c4400d528
dennyzhang <denny@dennyzhang.com>
2016-06-12 15:56:45 +0800

    add list_os_packages

---
+++ bash/list_os_packages/README.md
@@ -0,0 +1,3 @@
+List Installed Packages and Version
+===================================
+This tool can easily help us to detect difference in between two nodes.
---
+++ bash/list_os_packages/list_os_packages.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+##-------------------------------------------------------------------
+## @copyright 2016 DennyZhang.com
+## Licensed under MIT
+## https://raw.githubusercontent.com/DennyZhang/devops_public/master/LICENSE
+##
+## File : list_os_packages.sh
+## Author : Denny <denny@dennyzhang.com>
+## Description :
+## Sample:
+## --
+## Created : <2016-06-04>
+## Updated: Time-stamp: <2016-06-12 15:55:16>
+##-------------------------------------------------------------------
+. /etc/profile
+
+## File: list_os_packages.sh ends
