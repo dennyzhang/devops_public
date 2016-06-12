45a2278c71d2975fadd6a1e44c924342e1e3f165
dennyzhang <denny@dennyzhang.com>
2016-06-12 10:24:38 +0800

    dump output to data file

--- bash/dump_db_summary/dump_db_summary.sh
+++ bash/dump_db_summary/dump_db_summary.sh
@@ -10,7 +10,7 @@
 ## Sample:
 ## --
 ## Created : <2016-06-04>
-## Updated: Time-stamp: <2016-06-12 10:14:31>
+## Updated: Time-stamp: <2016-06-12 10:24:19>
 ##-------------------------------------------------------------------
 . /etc/profile
 
@@ -19,12 +19,13 @@
 function dump_couchbase_summary() {
     local cfg_file=${1?}
     local output_type=${2?}
+    local output_data_file=${3?}
     source "$cfg_file"
     # Get parameters from $cfg_file:
     #    server_ip, tcp_port, cb_username, cb_password
     if [ "$output_type" = "json" ]; then
-        command="curl -u ${cb_username}:${cb_passwd} http://${server_ip}:${tcp_port}/pools/default/buckets"
-        command="$command | python -m json.tool"
+        curl -u "${cb_username}:${cb_passwd}" "http://${server_ip}:${tcp_port}/pools/default/buckets" \
+            | python -m json.tool > "$output_data_file"
         echo "TODO"
     fi
 }
@@ -32,11 +33,15 @@ function dump_couchbase_summary() {
 function dump_elasticsearch_summary() {
     local cfg_file=${1?}
     local output_type=${2?}
+    local output_data_file=${3?}
+
     source "$cfg_file"
     # Get parameters from $cfg_file:
     #    server_ip, tcp_port
     if [ "$output_type" = "json" ]; then
-        curl "http://${server_ip}:${tcp_port}/_cat/shards?v"
+        # TODO: Change to json
+        curl "http://${server_ip}:${tcp_port}/_cat/shards?v" \
+             > "$output_data_file"
         echo "TODO"
     fi
 }
@@ -44,22 +49,29 @@ function dump_elasticsearch_summary() {
 function dump_mongodb_summary() {
     local cfg_file=${1?}
     local output_type=${2?}
+    local output_data_file=${3?}
+
     source "$cfg_file"
     if [ "$output_type" = "json" ]; then
         echo "TODO"
+        echo "output_data_file: $output_data_file"
     fi
 }
+
 ################################################################################
 cfg_dir=${1:-"/opt/devops/dump_db_summary/cfg_dir"}
+data_out_dir=${2:-"/opt/devops/dump_db_summary/data_out"}
 # support string/json
-output_type=${2:-"json"}
+output_type=${3:-"json"}
+
+[ -d "$data_out_dir" ] || mkdir -p "$data_out_dir"
 
 cd "$cfg_dir"
 for f in *.cfg; do
     db_name=${f%%.cfg}
     # Sample: $cfg_dir/mongodb.cfg -> dump_mongodb_summary mongodb.cfg
     fun_name="dump_${db_name}_summary"
-    command="$fun_name $f $output_type"
+    command="$fun_name $f $output_type $data_out_dir/${db_name}.${output_type}"
     echo "$command"
     $command
 done
