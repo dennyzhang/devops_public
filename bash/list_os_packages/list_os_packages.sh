8d56272a048a8c7f145f24c1c6cd13e314ff60f3
dennyzhang <denny@dennyzhang.com>
2016-06-13 16:50:51 +0800

    code refactoring

--- bash/list_os_packages/list_os_packages.sh
+++ bash/list_os_packages/list_os_packages.sh
@@ -10,7 +10,7 @@
 ## Sample:
 ## --
 ## Created : <2016-06-04>
-## Updated: Time-stamp: <2016-06-13 16:31:56>
+## Updated: Time-stamp: <2016-06-13 16:50:41>
 ##-------------------------------------------------------------------
 . /etc/profile
 
@@ -21,87 +21,106 @@ function fail_unless_root() {
         exit 1
     fi
 }
+function run_all_scenario() {
+    local output_dir=${1?}
+    local check_type=${2?}
+    local scenario_list=${3?}
+    for scenario in $scenario_list; do
+        "list_${scenario}_info" "$output_dir" "$check_type"
+    done
+}
+
 ################################################################################
+function list_os_info() {
+    local output_dir=${1?}
+    local check_type=${2?}
+    echo "Dump OS Basic Info"
+    uname -a
+}
+
 function list_package_info() {
+    local output_dir=${1?}
+    local check_type=${2?}
     command="dpkg -l"
     echo -e "\n=================== Run Command: $command"
     eval "$command"
 }
 
 function list_python_info() {
-    command="pip list"
-    echo -e "\n=================== Run Command: $command"
-    eval "$command"
+    local output_dir=${1?}
+    local check_type=${2?}
+    if which pip 2>/dev/null 1>&2; then
+        command="pip list"
+        echo -e "\n=================== Run Command: $command"
+        eval "$command"
+    else
+        echo "Warning list_python_info: nothing done, since no pip detected"
+    fi
 }
 
 function list_ruby_info() {
-    command="gem list"
-    echo -e "\n=================== Run Command: $command"
-    eval "$command"
+    local output_dir=${1?}
+    local check_type=${2?}
+    if which gem 2>/dev/null 1>&2; then
+        command="gem list"
+        echo -e "\n=================== Run Command: $command"
+        eval "$command"
+    else
+        echo "Warning list_ruby_info: nothing done, since no gem detected"
+    fi
 }
 
 function list_nodejs_info() {
-    command="npm list"
-    echo -e "\n=================== Run Command: $command"
-    eval "$command"
+    local output_dir=${1?}
+    local check_type=${2?}
+    if which npm 2>/dev/null 1>&2; then
+        command="npm list"
+        echo -e "\n=================== Run Command: $command"
+        eval "$command"
+    else
+        echo "Warning list_nodejs_info: nothing done, since no npm detected"
+    fi
 }
 
 function list_java_info() {
-    command="echo TODO"
-    echo -e "\n=================== Run Command: $command"
-    eval "$command"
-}
-
-################################################################################
-function list_basic_info() {
-    echo "Dump OS Basic Info"
-    uname -a
-    # TODO: OS version, cpu, memory
-}
-
-function list_all_info() {
-    list_basic_info
-    list_package_info
-    if which gem 2>/dev/null 1>&2; then
-        list_ruby_info
-    fi
-
-    if which pip 2>/dev/null 1>&2; then
-        list_python_info
-    fi
-
-    if which npm 2>/dev/null 1>&2; then
-        list_nodejs_info
-    fi
-
+    local output_dir=${1?}
+    local check_type=${2?}
     if which java 2>/dev/null 1>&2; then
-        list_java_info
+        command="echo TODO"
+        echo -e "\n=================== Run Command: $command"
+        eval "$command"
+    else
+        echo "Warning list_java_info: nothing done, since no java detected"
     fi
 }
+
 ################################################################################
 # Sample:
-#   list_os_packages.sh basic
-#   list_os_packages.sh python
-#   list_os_packages.sh all
+# list_os_packages.sh basic
+# list_os_packages.sh python
+# list_os_packages.sh all
 check_scenario=${1:-"basic"}
 output_dir=${2:-"/root/version.d"}
 
 fail_unless_root
-[ -d "$output_dir" ] || mkdir -p $output_dir
+[ -d "$output_dir" ] || mkdir -p "$output_dir"
 
-# defensive coding for not supported scenario
-scenario_list="package python ruby nodejs java"
-if [ "$check_scenario" = "all" ] || [ "$check_scenario" = "basic" ] \
-       || [ *"$check_scenario"* = "$scenario_list" ]; then
-    echo "yes"
-else
-    echo "ERROR: Not supported check scenario($check_scenario)."
-    echo "Supported Scenarios: $scenario_list basic all."
-    exit 1
-fi
+scenario_list="package python ruby nodejs java os"
 
-command="list_${check_scenario}_info"
-echo "Run function: $command"
+case $check_scenario in
+    all) run_all_scenario "$output_dir" "basic" "$scenario_list";;
+    basic) run_all_scenario "$output_dir" "full" "$scenario_list";;
+    *)
+        if [ *"$check_scenario"* = "$scenario_list" ]; then
+            command="list_${check_scenario}_info"
+            echo "Run function: $command"
+            "list_${check_scenario}_info" "$output_dir" "full"
+        else
+            echo "ERROR: Not supported check scenario($check_scenario)."
+            echo "Supported Scenarios: $scenario_list basic all."
+            exit 1
+        fi
+        ;;
+esac
 
-eval "$command"
 ## File: list_os_packages.sh ends
