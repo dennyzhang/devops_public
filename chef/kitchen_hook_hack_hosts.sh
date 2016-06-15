68b4516380fb9dae0952578cba54e8f3fd14dd59
dennyzhang <denny@dennyzhang.com>
2016-06-15 13:48:12 +0800

    use sudo for kitchen hoooks

--- chef/kitchen_hook_hack_hosts.sh
+++ chef/kitchen_hook_hack_hosts.sh
@@ -4,9 +4,9 @@ set +e
 # get list of nodes
 hosts_list=""
 for node in $(kitchen list | grep -v '^Instance' | awk -F' ' '{print $1}'); do
-    output=$(kitchen exec "$node" -c "/sbin/ifconfig eth0 | grep 'inet addr:'")
+    output=$(kitchen exec "$node" -c "sudo /sbin/ifconfig eth0 | grep 'inet addr:'")
     ip=$(echo "$output" | grep -v LC_ALL | grep -v '^---' | cut -d: -f2 | awk '{print $1}')
-    output=$(kitchen exec "$node" -c "hostname")
+    output=$(kitchen exec "$node" -c "sudo hostname")
     hostname=$(echo "$output" | grep -v LC_ALL | grep -v '^---')
     # trim whitespace
     hostname=$(echo "${hostname}" | sed -e 's/^[ \t]*//')
@@ -24,13 +24,13 @@ do
     domain=${host_split[1]}
 
     for node in $(kitchen list | grep -v '^Instance' | awk -F' ' '{print $1}'); do
-        kitchen exec "$node" -c "cp -f /etc/hosts /root/hosts"
-        if kitchen exec "$node" -c "grep ${domain} /root/hosts"; then
+        kitchen exec "$node" -c "sudo cp -f /etc/hosts /root/hosts"
+        if kitchen exec "$node" -c "sudo grep ${domain} /root/hosts"; then
             command="sed -i \"/${domain}/c\\${ip}    ${domain}\" /root/hosts"
         else
             command="echo \"${ip}    ${domain}\" >> /root/hosts"
         fi
-        kitchen exec "$node" -c "$command"
+        kitchen exec "$node" -c "sudo $command"
         kitchen exec "$node" -c "sudo cp -f /root/hosts /etc/hosts"
     done
 done
