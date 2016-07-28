a53375fdcde9a6348af473fb83babf053f68e62d
dennyzhang <denny@dennyzhang.com>
2016-07-28 10:04:00 +0800

    implement code logic

--- jenkins_scripts/report/refresh_db_report.sh
+++ jenkins_scripts/report/refresh_db_report.sh
@@ -9,12 +9,14 @@
 ## Description :
 ## --
 ## Created : <2015-09-24>
-## Updated: Time-stamp: <2016-07-28 09:56:36>
+## Updated: Time-stamp: <2016-07-28 10:03:11>
 ##-------------------------------------------------------------------
 ################################################################################################
 ## env variables:
 ##       env_parameters:
 ##           export SSH_DOCKER_DAEMON="ssh root@172.17.0.1"
+##           export KIBANA_DASHBOARD_URL="http://104.131.129.100:5601"
+##           export DESTROY_CONTAINER=false
 ################################################################################################
 . /etc/profile
 [ -n "$DOWNLOAD_TAG_NAME" ] || export DOWNLOAD_TAG_NAME="tag_v2"
@@ -27,11 +29,18 @@ bash /var/lib/devops/refresh_common_library.sh "2593132520" "/var/lib/devops/dev
      "${DOWNLOAD_PREFIX}/common_library/devops_common_library.sh"
 . /var/lib/devops/devops_common_library.sh
 ################################################################################################
-export SSH_DOCKER_DAEMON="ssh root@172.17.0.1"
+ensure_variable_isset "SSH_DOCKER_DAEMON is not given " "$SSH_DOCKER_DAEMON"
+ensure_variable_isset "KIBANA_DASHBOARD_URL is not given " "$KIBANA_DASHBOARD_URL"
+[ -n "$DESTROY_CONTAINER" ] || DESTROY_CONTAINER=false
 
 echo "destroy old docker container of data-report"
 $SSH_DOCKER_DAEMON docker stop data-report || true
 $SSH_DOCKER_DAEMON docker rm data-report || true
+
+if $DESTROY_CONTAINER; then
+    echo "Quit after container destroy"
+    exit 0
+fi
 
 echo "start elk data report container and services inside"
 $SSH_DOCKER_DAEMON docker run -t -d --name data-report --privileged -p 5601:5601 denny/elk:datareport /usr/sbin/sshd -D
@@ -44,6 +53,5 @@ echo "Download and inject data file"
 $SSH_DOCKER_DAEMON docker exec -t data-report wget -O /tmp/db_summary_report.txt http://repo.fluigdata.com:18000/prodenv_db_summary_report/db_summary_report.txt
 $SSH_DOCKER_DAEMON docker exec -t data-report cat /tmp/db_summary_report.txt >> /var/log/data_report.log
 
-# http://104.131.129.100:5601
-# TODO: whether to shutdown container
+echo "Check DB Report: $KIBANA_DASHBOARD_URL"
 ## File : refresh_db_report.sh ends
