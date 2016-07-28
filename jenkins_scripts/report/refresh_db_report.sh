26068a9354d442cc7507e282245882ae691b85ce
dennyzhang <denny@dennyzhang.com>
2016-07-28 10:07:21 +0800

    quote commands

--- jenkins_scripts/report/refresh_db_report.sh
+++ jenkins_scripts/report/refresh_db_report.sh
@@ -9,7 +9,7 @@
 ## Description :
 ## --
 ## Created : <2015-09-24>
-## Updated: Time-stamp: <2016-07-28 10:03:11>
+## Updated: Time-stamp: <2016-07-28 10:07:08>
 ##-------------------------------------------------------------------
 ################################################################################################
 ## env variables:
@@ -29,6 +29,8 @@ bash /var/lib/devops/refresh_common_library.sh "2593132520" "/var/lib/devops/dev
      "${DOWNLOAD_PREFIX}/common_library/devops_common_library.sh"
 . /var/lib/devops/devops_common_library.sh
 ################################################################################################
+source_string "$env_parameters"
+
 ensure_variable_isset "SSH_DOCKER_DAEMON is not given " "$SSH_DOCKER_DAEMON"
 ensure_variable_isset "KIBANA_DASHBOARD_URL is not given " "$KIBANA_DASHBOARD_URL"
 [ -n "$DESTROY_CONTAINER" ] || DESTROY_CONTAINER=false
@@ -45,13 +47,13 @@ fi
 echo "start elk data report container and services inside"
 $SSH_DOCKER_DAEMON docker run -t -d --name data-report --privileged -p 5601:5601 denny/elk:datareport /usr/sbin/sshd -D
 
-$SSH_DOCKER_DAEMON docker exec -t data-report service logstash start
-$SSH_DOCKER_DAEMON docker exec -t data-report service elasticsearch start
-$SSH_DOCKER_DAEMON docker exec -t data-report service kibana4 start
+$SSH_DOCKER_DAEMON docker exec -t data-report "service logstash start"
+$SSH_DOCKER_DAEMON docker exec -t data-report "service elasticsearch start"
+$SSH_DOCKER_DAEMON docker exec -t data-report "service kibana4 start"
 
 echo "Download and inject data file"
-$SSH_DOCKER_DAEMON docker exec -t data-report wget -O /tmp/db_summary_report.txt http://repo.fluigdata.com:18000/prodenv_db_summary_report/db_summary_report.txt
-$SSH_DOCKER_DAEMON docker exec -t data-report cat /tmp/db_summary_report.txt >> /var/log/data_report.log
+$SSH_DOCKER_DAEMON docker exec -t data-report "wget -O /tmp/db_summary_report.txt http://repo.fluigdata.com:18000/prodenv_db_summary_report/db_summary_report.txt"
+$SSH_DOCKER_DAEMON docker exec -t data-report "cat /tmp/db_summary_report.txt >> /var/log/data_report.log"
 
 echo "Check DB Report: $KIBANA_DASHBOARD_URL"
 ## File : refresh_db_report.sh ends
