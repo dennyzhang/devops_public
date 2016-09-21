a6fba912701c6e9d05acced522f54ec14ce0dede
dennyzhang <denny@dennyzhang.com>
2016-09-21 11:12:35 +0800

    update script

--- test/fault_test.sh
+++ test/fault_test.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#!/bin/bash -ex
+function shell_exit() {
+    errcode=$?
+    if [ $errcode -eq 0 ]; then
+        log "Action succeeds."
+    else
+        log "Action Fails."
+    fi
+    exit $errcode
+}
+
+trap shell_exit SIGHUP SIGINT SIGTERM 0
+
 command="ssh root@104.131.129.100 ls /tmp/abab"
 echo "$command"
 $command
