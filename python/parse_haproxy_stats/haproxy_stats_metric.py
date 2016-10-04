a0a72f46928e60b0f9b61895d6bc37eafdfaf67d
dennyzhang <denny@dennyzhang.com>
2016-10-04 20:21:54 +0800

    update python

--- python/parse_haproxy_stats/haproxy_stats_metric.py
+++ python/parse_haproxy_stats/haproxy_stats_metric.py
@@ -10,10 +10,13 @@
 ## Description : A Python module to parse haproxy stats
 ## --
 ## Created : <2016-10-04>
-## Updated: Time-stamp: <2016-10-04 20:15:33>
+## Updated: Time-stamp: <2016-10-04 20:21:46>
 ##-------------------------------------------------------------------
 import parse_haproxy_stats
 import datetime
+import argparse
+import commands
+
 def get_timestamp():
     return "[%s +0000]" % \
         (datetime.datetime.utcnow().strftime("%d/%b/%Y:%H:%M:%S"))
@@ -27,8 +30,22 @@ def haproxy_stats_metric(stat_output, timestamp):
     for field in 'scur,smax'.split(','):
         print "%s %s %s %s" % (timestamp, 'HTTPCode', field, haproxy_dict[field])
 
+# python ./haproxy_stats_metric.py --haproxy_stats_cmd "echo 'show stat' | nc -U /var/run/haproxy/admin.sock | grep 'backend-https,BACKEND'"
 if __name__=='__main__':
-    stat_output = "backend-https,BACKEND,0,0,0,9,200,276978,2746128724,678805060,0,0,,47,6,3,0,UP,1,1,0,,22,101965,3120,,1,5,0,,276932,,1,0,,27,,,,0,239978,7435,29459,102,4,,,,,16,0,0,0,0,0,106,,,0,57,31,478,"
-    timestamp = get_timestamp()
-    haproxy_stats_metric(stat_output, timestamp)
+    parser = argparse.ArgumentParser()
+    default_haproxy_stats_cmd \
+        = "echo 'show stat' | nc -U /var/run/haproxy/admin.sock | grep 'backend-https,BACKEND'"
+    parser.add_argument('--haproxy_stats_cmd', default=default_haproxy_stats_cmd, \
+                        required=True, help="Command to get haproxy stats", type=str)
+
+    l = parser.parse_args()
+    cmd = l.default_haproxy_stats_cmd
+    status, output = commands.getstatusoutput(cmd)
+    if(status == 0):
+        stat_output = output.strip()
+        timestamp = get_timestamp()
+        haproxy_stats_metric(stat_output, timestamp)
+    else:
+        msg = "Error to run cmd: %s\n. output: %s" % (cmd, output)
+        raise Exception(msg)
 ## File : haproxy_stats_metric.py ends
