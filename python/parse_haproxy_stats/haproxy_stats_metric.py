35cbc1eb4527a7d7e9183ba5f6e86357a6aef828
dennyzhang <denny@dennyzhang.com>
2016-10-05 09:06:34 +0800

    change format

--- python/parse_haproxy_stats/haproxy_stats_metric.py
+++ python/parse_haproxy_stats/haproxy_stats_metric.py
@@ -10,7 +10,7 @@
 ## Description : A Python module to parse haproxy stats
 ## --
 ## Created : <2016-10-04>
-## Updated: Time-stamp: <2016-10-04 20:49:05>
+## Updated: Time-stamp: <2016-10-05 09:06:27>
 ##-------------------------------------------------------------------
 import parse_haproxy_stats
 import datetime
@@ -25,10 +25,10 @@ def haproxy_stats_metric(stat_output, timestamp):
     haproxy_dict = parse_haproxy_stats.parse_haproxy_stats(stat_output)
 
     for field in 'hrsp_2xx,hrsp_4xx,hrsp_5xx'.split(','):
-        print "%s %s %s %s" % (timestamp, 'HTTPCode', field, haproxy_dict[field])
+        print "%s %s %s %s" % (timestamp, field, 'HTTPCode', haproxy_dict[field])
 
     for field in 'scur,smax'.split(','):
-        print "%s %s %s %s" % (timestamp, 'SessionThread', field, haproxy_dict[field])
+        print "%s %s %s %s" % (timestamp, field, 'SessionThread', haproxy_dict[field])
 
 # python ./haproxy_stats_metric.py --haproxy_stats_cmd "echo 'show stat' | nc -U /var/run/haproxy/admin.sock | grep 'backend-https,BACKEND'"
 if __name__=='__main__':
