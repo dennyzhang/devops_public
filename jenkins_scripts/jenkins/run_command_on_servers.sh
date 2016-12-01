51a7747d78c3c93d74f31baca9734394f5c6907b
dennyzhang <denny@dennyzhang.com>
2016-12-01 17:28:47 +0800

    data cleanup

--- jenkins_scripts/jenkins/run_command_on_servers.sh
+++ jenkins_scripts/jenkins/run_command_on_servers.sh
@@ -9,34 +9,39 @@
 ## Description :
 ## --
 ## Created : <2016-04-13>
-## Updated: Time-stamp: <2016-12-01 17:02:58>
+## Updated: Time-stamp: <2016-12-01 17:28:30>
 ##-------------------------------------------------------------------
+
+################################################################################################
 ## env variables:
-##       server_list: ip-1
-##                    ip-2
+##       server_list: ip-1:port-1:root
+##                    ip-2:port-2:root
 ##       command_list:
 ##        cat /etc/hosts
 ##        ls /opt/
 ##
 ##       env_parameters:
 ##          export EXIT_NODE_CONNECT_FAIL=false
-##          export SSH_USERNAME="root"
 ##          export ssh_key_file="/var/lib/jenkins/.ssh/id_rsa"
 ################################################################################################
 . /etc/profile
-[ -n "$DOWNLOAD_TAG_NAME" ] || export DOWNLOAD_TAG_NAME="tag_v2"
+[ -n "$DOWNLOAD_TAG_NAME" ] || export DOWNLOAD_TAG_NAME="tag_v1"
 export DOWNLOAD_PREFIX="https://raw.githubusercontent.com/DennyZhang/devops_public/${DOWNLOAD_TAG_NAME}"
 if [ ! -f /var/lib/devops/refresh_common_library.sh ]; then
     [ -d /var/lib/devops/ ] || (sudo mkdir -p  /var/lib/devops/ && sudo chmod 777 /var/lib/devops)
     wget -O /var/lib/devops/refresh_common_library.sh "$DOWNLOAD_PREFIX/common_library/refresh_common_library.sh"
 fi
-bash /var/lib/devops/refresh_common_library.sh "397508245" "/var/lib/devops/devops_common_library.sh" \
+bash /var/lib/devops/refresh_common_library.sh "1597538024" "/var/lib/devops/devops_common_library.sh" \
      "${DOWNLOAD_PREFIX}/common_library/devops_common_library.sh"
 . /var/lib/devops/devops_common_library.sh
 ################################################################################################
 function shell_exit() {
     errcode=$?
-    rm -rf "$tmp_file" "$ansible_host_file"
+    rm -rf "$tmp_file"
+    if [ -n "$failed_servers" ]; then
+        echo "=============== Failed server: $failed_servers"
+        exit 1
+    fi
     exit $errcode
 }
 ################################################################################################
@@ -45,33 +50,43 @@ source_string "$env_parameters"
 
 [ -n "$ssh_key_file" ] || export ssh_key_file="/var/lib/jenkins/.ssh/id_rsa"
 [ -n "$EXIT_NODE_CONNECT_FAIL" ] || export EXIT_NODE_CONNECT_FAIL=false
-[ -n "$SSH_USERNAME" ] || export SSH_USERNAME="root"
 
-echo "server_list: $server_list"
 server_list=$(string_strip_comments "$server_list")
 server_list=$(string_strip_whitespace "$server_list")
-command_list=$(string_strip_comments "$command_list")
+command=$(string_strip_comments "$command")
 
-if ! which ansible 1>/dev/null 2>&1; then
-    sudo apt-get install -y ansible
-fi
+# Input Parameters check
+verify_comon_jenkins_parameters
 
+failed_servers=""
 # Dump bash command to scripts
 current_filename=$(basename "${0}")
-ansible_host_file="/tmp/${current_filename}_$$_hosts"
 tmp_file="/tmp/${current_filename}_$$"
 cat > "$tmp_file" <<EOF
 $command_list
 EOF
 
-cat > "$ansible_host_file" <<EOF
-$server_list
-EOF
+# TODO: verify command_list is valid, in case users have wrong input
+IFS=$'\n'
+for server in ${server_list}; do
+    unset IFS
+    server_split=(${server//:/ })
+    ssh_server_ip=${server_split[0]}
+    ssh_port=${server_split[1]}
+    ssh_username=${server_split[2]}
+    [ -n "$ssh_username" ] || ssh_username="root"
 
-echo "==================== Parallel run command By Ansible:"
-cat "$tmp_file"
-echo "===================="
+    # TODO: support blacklist and whitelist mechanism
+    ssh_command="scp -P $ssh_port -i $ssh_key_file -o StrictHostKeyChecking=no $tmp_file $ssh_username@$ssh_server_ip:/$tmp_file"
+    $ssh_command
 
-sudo ansible all -i "$ansible_host_file" -m script -a "$tmp_file" -u "$SSH_USERNAME" "--private-key=$ssh_key_file"
-sudo ansible all -i "$ansible_host_file" -m command -a "rm -rf $tmp_file" -u "$SSH_USERNAME" "--private-key=$ssh_key_file"
+    ssh_connect="ssh -i $ssh_key_file -p $ssh_port -o StrictHostKeyChecking=no $ssh_username@$ssh_server_ip"
+    ssh_command="$ssh_connect \"bash -ex $tmp_file\""
+    echo -e "\n=============== Run Command on $ssh_server_ip:$ssh_port"
+    if ! eval "$ssh_command"; then
+        failed_servers="${failed_servers} ${ssh_server_ip}:${ssh_port}"
+    fi
+    ssh_command="$ssh_connect rm -rf $tmp_file"
+    eval "$ssh_command"
+done
 ## File : run_command_on_servers.sh ends
