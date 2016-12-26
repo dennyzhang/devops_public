e46982a09a946481f0ad702dab9b92c46cc4559c
dennyzhang <denny@dennyzhang.com>
2016-12-26 12:04:45 +0800

    shellcheck fix

--- jenkins_scripts/firewall/ufw_add_node_to_cluster.sh
+++ jenkins_scripts/firewall/ufw_add_node_to_cluster.sh
@@ -9,19 +9,22 @@
 ## Description :
 ## --
 ## Created : <2016-12-26>
-## Updated: Time-stamp: <2016-12-26 12:03:01>
+## Updated: Time-stamp: <2016-12-26 12:04:34>
 ##-------------------------------------------------------------------
 allow_ip_list=${1?}
+allow_tcp_port=${2:-"2702 22"}
 
-iptables-save > /root/$(date +'%Y%m%d_%H%M%S')_rules.v4
+iptables-save > "/root/$(date +'%Y%m%d_%H%M%S')_rules.v4"
 iptables -F; iptables -X
 echo 'y' | ufw reset
 echo 'y' | ufw enable
 ufw default deny incoming
 ufw default deny forward
-ufw allow 2702/tcp
+for port in $allow_tcp_port; do
+    ufw allow "$port/tcp"
+done
 
-for ip in "$allow_ip_list"; do
+for ip in $allow_ip_list; do
     ufw allow from "$ip"
 done
 ## File : ufw_add_node_to_cluster.sh ends
