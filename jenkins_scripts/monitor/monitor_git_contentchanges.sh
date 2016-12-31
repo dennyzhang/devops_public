c3ebd43e68f722ba4aefb97caccbde43aaefbba8
dennyzhang <denny@dennyzhang.com>
2016-12-31 11:35:23 -0600

    code cleanup

--- jenkins_scripts/monitor/monitor_git_contentchanges.sh
+++ jenkins_scripts/monitor/monitor_git_contentchanges.sh
@@ -9,7 +9,7 @@
 ## Description :
 ## --
 ## Created : <2016-12-31>
-## Updated: Time-stamp: <2016-12-31 11:29:48>
+## Updated: Time-stamp: <2016-12-31 11:35:16>
 ##-------------------------------------------------------------------
 ################################################################################################
 ## env variables:
@@ -158,7 +158,6 @@ else
             command="git diff $old_sha $new_sha $file | grep -iE '^- |^\+ '"
             output=$(eval "$command")
             for pattern in ${monitor_pattern_list[*]}; do
-                # echo TODO
                 if echo "$output" | grep "$pattern" 1>/dev/null 2>&1; then
                     echo -e "========== ERROR subscribed changes detected: $file"
                     subscribed_change_detected=true
