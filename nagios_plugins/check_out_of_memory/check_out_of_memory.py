56a35df747f6f8e08f315033d16a1ca1854d1f27
dennyzhang <denny@dennyzhang.com>
2017-03-14 14:00:33 -0500

    update code skeleton

--- nagios_plugins/check_out_of_memory/check_out_of_memory.py
+++ nagios_plugins/check_out_of_memory/check_out_of_memory.py
@@ -1,4 +1,5 @@
-#!/bin/bash -e
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
 ##-------------------------------------------------------------------
 ## @copyright 2017 DennyZhang.com
 ## Licensed under MIT 
@@ -6,10 +7,28 @@
 ##
 ## File : check_out_of_memory.py
 ## Author : Denny <denny@dennyzhang.com>
-## Description :
+## Description : Detect whether OOM(Out Of Memory) has happened in the previous several hours
 ## --
 ## Created : <2017-02-28>
-## Updated: Time-stamp: <2017-02-28 15:05:37>
+## Updated: Time-stamp: <2017-03-14 14:00:21>
 ##-------------------------------------------------------------------
-# TODO: to be implemented. Check: http://www.dennyzhang.com/monitor_oom/
+# Check: http://www.dennyzhang.com/monitor_oom/
+import argparse
+import sys
+
+NAGIOS_OK_ERROR=0
+NAGIOS_EXIT_ERROR=2
+
+def get_oom_entry():
+    oom_list = []
+    return oom_list
+
+def filter_entry_by_datetime(oom_list, datetime, offset_hours):
+    ret_list = []
+    return ret_list
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--es_host', required=False, \
+                        help="server ip or hostname for elasticsearch instance. Default value is ip of eth0", type=str)
 ## File : check_out_of_memory.py ends
