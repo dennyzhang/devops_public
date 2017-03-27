c251ebd06aad2b9e0a05752767c2b0a1297f2623
dennyzhang <denny@dennyzhang.com>
2017-03-27 14:01:56 -0500

    shellcheck fix

--- bash/es_reindex.sh
+++ bash/es_reindex.sh
@@ -4,7 +4,7 @@
 ## Description :
 ## --
 ## Created : <2017-03-27>
-## Updated: Time-stamp: <2017-03-27 14:01:36>
+## Updated: Time-stamp: <2017-03-27 14:01:46>
 ##-------------------------------------------------------------------
 old_index_name=${1?}
 
@@ -75,7 +75,7 @@ time curl -XPOST "http://${es_ip}:9200/_aliases" -d "
 }"
 
 # List alias
-curl -XPGET "http://${es_ip}:9200/_aliases?pretty" | grep -C 10 $(echo "$old_index_name" | sed "s/.*-index-//g")
+curl -XPGET "http://${es_ip}:9200/_aliases?pretty" | grep -C 10 "$(echo "$old_index_name" | sed "s/.*-index-//g")"
 
 # Close index
 curl -XPOST "http://${es_ip}:9200/${old_index_name}/_close"
