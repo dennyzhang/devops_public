fa3fe46143063b0a9d472b046c1cb82ad0435ced
dennyzhang <denny@dennyzhang.com>
2017-03-28 23:09:05 -0500

    log the timestamp

--- bash/es_reindex.sh
+++ bash/es_reindex.sh
@@ -4,7 +4,7 @@
 ## Description :
 ## --
 ## Created : <2017-03-27>
-## Updated: Time-stamp: <2017-03-28 11:15:11>
+## Updated: Time-stamp: <2017-03-28 22:58:30>
 ##-------------------------------------------------------------------
 old_index_name=${1?}
 shard_count=${2:-"10"}
@@ -37,6 +37,16 @@ if [ "$alias_index_name" = "$old_index_name" ]; then
     exit 1
 fi
 
+function log() {
+    local msg=$*
+    date_timestamp=$(date +['%Y-%m-%d %H:%M:%S'])
+    echo -ne "$date_timestamp $msg\n"
+    
+    if [ -n "$LOG_FILE" ]; then
+        echo -ne "$date_timestamp $msg\n" >> "$LOG_FILE"
+    fi
+}
+
 ##-------------------------------------------------------------------
 # Sample test:
 # export old_index_name="staging-index-46078234297e400a1648d9c427dc8c4b"
@@ -47,12 +57,12 @@ fi
 # export es_ip=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 # export es_port=9200
 
-echo "old_index_name: $old_index_name, new_index_name: $new_index_name"
+log "old_index_name: $old_index_name, new_index_name: $new_index_name"
 
-echo "List all indices"
+log "List all indices"
 time curl -XGET "http://${es_ip}:${es_port}/_cat/indices?v"
 
-echo "create new index with proper shards and replicas"
+log "create new index with proper shards and replicas"
 time curl -XPUT "http://${es_ip}:${es_port}/${new_index_name}?pretty" -d "
     {
        \"settings\" : {
@@ -63,10 +73,10 @@ time curl -XPUT "http://${es_ip}:${es_port}/${new_index_name}?pretty" -d "
    }
 }"
 
-echo "Get the setting of the new index"
+log "Get the setting of the new index"
 time curl -XGET "http://${es_ip}:${es_port}/${new_index_name}/_settings?pretty"
 
-echo "Reindex index. Attention: this will take a very long time, if the index is big"
+log "Reindex index. Attention: this will take a very long time, if the index is big"
 time curl -XPOST "http://${es_ip}:${es_port}/_reindex?pretty" -d "
     {
     \"conflicts\": \"proceed\",
@@ -80,10 +90,10 @@ time curl -XPOST "http://${es_ip}:${es_port}/_reindex?pretty" -d "
 }"
 
 # We can start a new terminal and check reindex status
-echo "Get all re-index tasks"
+log "Get all re-index tasks"
 time curl -XGET "http://${es_ip}:${es_port}/_tasks?detailed=true&actions=*reindex&pretty"
 
-echo "Add index to existing alias and remove old index from that alias. alias: $alias_index_name"
+log "Add index to existing alias and remove old index from that alias. alias: $alias_index_name"
 time curl -XPOST "http://${es_ip}:${es_port}/_aliases" -d "
 {
     \"actions\": [
@@ -107,7 +117,7 @@ curl -XPGET "http://${es_ip}:${es_port}/_aliases?pretty" | grep -C 10 "$(echo "$
 # Delete index
 # curl -XDELETE "http://${es_ip}:${es_port}/${old_index_name}?pretty"
 
-echo "List all indices"
+log "List all indices"
 time curl -XGET "http://${es_ip}:${es_port}/_cat/indices?v"
 
 ## File : es_reindex.sh ends
