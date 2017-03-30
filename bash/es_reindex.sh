e786471dcfa41a7edcea4cb546db9d630e2d76cd
dennyzhang <denny@dennyzhang.com>
2017-03-29 19:09:30 -0500

    improve error handling

--- bash/es_reindex.sh
+++ bash/es_reindex.sh
@@ -6,7 +6,7 @@
 ##               Check more: https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-reindex.html
 ## --
 ## Created : <2017-03-27>
-## Updated: Time-stamp: <2017-03-29 13:14:26>
+## Updated: Time-stamp: <2017-03-29 19:09:22>
 ##-------------------------------------------------------------------
 old_index_name=${1?}
 shard_count=${2:-"10"}
@@ -16,7 +16,7 @@ replica_count=${5:-"2"}
 es_ip=${6:-""}
 es_port=${7:-"9200"}
 
-log_file="/var/log/es_reindex.log"
+log_file="/var/log/es_reindex_sh.log"
 ##-------------------------------------------------------------------
 # Configure default value, if not given
 if [ -z "$alias_index_name" ]; then
@@ -36,7 +36,7 @@ fi
 ##-------------------------------------------------------------------
 # Precheck
 if [ "$alias_index_name" = "$old_index_name" ]; then
-    echo "ERROR: wrong parameter. old_index_name and alias_index_name can't be the same" >> "$log_file"
+    echo "ERROR: wrong parameter. old_index_name and alias_index_name can't be the same" | tee -a "$log_file"
     exit 1
 fi
 
@@ -52,15 +52,26 @@ fi
 # export replica_count=0
 # export es_ip=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 # export es_port=9200
+# export log_file="/var/log/es_reindex_sh.log"
+#
+# List indices:
+# curl $es_ip:9200/_cat/indices?v
 
-echo "$(date +['%Y-%m-%d %H:%M:%S']) old_index_name: $old_index_name, new_index_name: $new_index_name" >> "$log_file"
+echo "$(date +['%Y-%m-%d %H:%M:%S']) old_index_name: $old_index_name, new_index_name: $new_index_name" | tee -a "$log_file"
 
-echo "$(date +['%Y-%m-%d %H:%M:%S']) List all indices" >> "$log_file"
+echo "$(date +['%Y-%m-%d %H:%M:%S']) List all indices" | tee -a "$log_file"
 time curl -XGET "http://${es_ip}:${es_port}/_cat/indices?v" | tee -a "$log_file"
 
 # TODO: verify whether ES index exists
 
-echo "$(date +['%Y-%m-%d %H:%M:%S']) create new index with proper shards and replicas" >> "$log_file"
+if curl -XGET "http://${es_ip}:${es_port}/${new_index_name}?pretty" | grep "\"status\" : 404"; then
+    echo "$(date +['%Y-%m-%d %H:%M:%S']) index doesn't exist, which is good." | tee -a "$log_file"
+else
+    echo "$(date +['%Y-%m-%d %H:%M:%S']) ERROR: index(${new_index_name}) already exists" | tee -a "$log_file"
+    exit 1
+fi
+
+echo "$(date +['%Y-%m-%d %H:%M:%S']) create new index with proper shards and replicas" | tee -a "$log_file"
 time curl -XPUT "http://${es_ip}:${es_port}/${new_index_name}?pretty" -d "
     {
        \"settings\" : {
@@ -71,12 +82,19 @@ time curl -XPUT "http://${es_ip}:${es_port}/${new_index_name}?pretty" -d "
    }
 }" | tee -a "$log_file"
 
-echo "$(date +['%Y-%m-%d %H:%M:%S']) Get the setting of the new index" >> "$log_file"
+if tail -n 5 "$log_file" | grep "\"acknowledged\" : true"; then
+    echo "$(date +['%Y-%m-%d %H:%M:%S']) keep going with the following process" | tee -a "$log_file"
+else
+    echo "$(date +['%Y-%m-%d %H:%M:%S']) ERROR to run previous curl command" | tee -a "$log_file"
+    exit 1
+fi
+
+echo "$(date +['%Y-%m-%d %H:%M:%S']) Get the setting of the new index" | tee -a "$log_file"
 time curl -XGET "http://${es_ip}:${es_port}/${new_index_name}/_settings?pretty" | tee -a "$log_file"
 
 # TODO: make sure all shards(primary/replica) for this new index are good.
 
-echo "$(date +['%Y-%m-%d %H:%M:%S']) Reindex index. Attention: this will take a very long time, if the index is big" >> "$log_file"
+echo "$(date +['%Y-%m-%d %H:%M:%S']) Reindex index. Attention: this will take a very long time, if the index is big" | tee -a "$log_file"
 time curl -XPOST "http://${es_ip}:${es_port}/_reindex?pretty" -d "
     {
     \"conflicts\": \"proceed\",
@@ -92,12 +110,12 @@ time curl -XPOST "http://${es_ip}:${es_port}/_reindex?pretty" -d "
 # TODO: confirm status, before proceed
 
 # We can start a new terminal and check reindex status
-echo "$(date +['%Y-%m-%d %H:%M:%S']) Get all re-index tasks" >> "$log_file"
+echo "$(date +['%Y-%m-%d %H:%M:%S']) Get all re-index tasks" | tee -a "$log_file"
 time curl -XGET "http://${es_ip}:${es_port}/_tasks?detailed=true&actions=*reindex&pretty" | tee -a "$log_file"
 
 # TODO: don't add alias
 
-echo "$(date +['%Y-%m-%d %H:%M:%S']) Add index to existing alias and remove old index from that alias. alias: $alias_index_name" >> "$log_file"
+echo "$(date +['%Y-%m-%d %H:%M:%S']) Add index to existing alias and remove old index from that alias. alias: $alias_index_name" | tee -a "$log_file"
 time curl -XPOST "http://${es_ip}:${es_port}/_aliases" -d "
 {
     \"actions\": [
@@ -112,9 +130,9 @@ time curl -XPOST "http://${es_ip}:${es_port}/_aliases" -d "
     ]
 }" | tee -a "$log_file"
 
-# echo "$(date +['%Y-%m-%d %H:%M:%S']) List all alias" >> "$log_file"
+# echo "$(date +['%Y-%m-%d %H:%M:%S']) List all alias" | tee -a "$log_file"
 # curl -XGET "http://${es_ip}:${es_port}/_aliases?pretty" \
-#       | grep -C 10 "$(echo "$old_index_name" | sed "s/.*-index-//g")" | tee -a "$log_file"
+# | grep -C 10 "$(echo "$old_index_name" | sed "s/.*-index-//g")" | tee -a "$log_file"
 
 # Close index: only after no requests access old index, we can close it
 # curl -XPOST "http://${es_ip}:${es_port}/${old_index_name}/_close" | tee -a "$log_file"
@@ -122,6 +140,6 @@ time curl -XPOST "http://${es_ip}:${es_port}/_aliases" -d "
 # Delete index
 # curl -XDELETE "http://${es_ip}:${es_port}/${old_index_name}?pretty" | tee -a "$log_file"
 
-echo "$(date +['%Y-%m-%d %H:%M:%S']) List all indices" >> "$log_file"
+echo "$(date +['%Y-%m-%d %H:%M:%S']) List all indices" | tee -a "$log_file"
 time curl -XGET "http://${es_ip}:${es_port}/_cat/indices?v" | tee -a "$log_file"
 ## File : es_reindex.sh ends
