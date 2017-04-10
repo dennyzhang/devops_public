242facf9898f04627a2d9163e787c738f103dc49
dennyzhang <denny@dennyzhang.com>
2017-04-09 22:38:58 -0500

    fix typo

--- python/enforce_shellcheck/enforce_shellcheck.py
+++ python/enforce_shellcheck/enforce_shellcheck.py
@@ -10,7 +10,7 @@
 ## Description :
 ## --
 ## Created : <2017-04-02>
-## Updated: Time-stamp: <2017-04-09 22:18:57>
+## Updated: Time-stamp: <2017-04-09 22:38:23>
 ##-------------------------------------------------------------------
 import argparse
 import sys
@@ -52,7 +52,7 @@ def run_check(file_list, check_pattern):
     return has_error
 ################################################################################
 #
-# wget -O /tmp/enforce_shellcheck.py https://raw.githubusercontent.com/DennyZhang/devops_public/tag_v5/python/enforce_pylint_check/enforce_shellcheck.py
+# wget -O /tmp/enforce_shellcheck.py https://raw.githubusercontent.com/DennyZhang/devops_public/tag_v5/python/enforce_shellcheck/enforce_shellcheck.py
 # python /tmp/enforce_shellcheck.py --code_dir devops_code/devops_public
 ################################################################################
 
