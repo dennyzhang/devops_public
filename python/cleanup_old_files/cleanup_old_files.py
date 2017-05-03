55d7a5bfd0fea2f0db76932a4c314341d68af593
dennyzhang <denny@dennyzhang.com>
2017-05-03 17:35:16 -0500

    update code skeleton

--- python/cleanup_old_files/cleanup_old_files.py
+++ python/cleanup_old_files/cleanup_old_files.py
@@ -12,11 +12,11 @@
 ## Sample:
 ##    # Remove files: Check /opt/app and remove files naming "app-.*-SNAPSHOT.jar". But keep latest 2 copies
 ##    python cleanup_old_files.py --working_dir "/opt/app" --filename_pattern "app-.*-SNAPSHOT.jar" \
-##               --cleanup_type file --min_copies 3
+##               --cleanup_type file --min_copies 3 --min_size_mb 10
 ##
-##    # Remove files: Only cleanup files over 100MB
+##    # Remove files: Only cleanup files over 200MB
 ##    python cleanup_old_files.py --working_dir "/opt/app" --filename_pattern "app-.*-SNAPSHOT.jar" \
-##               --cleanup_type file --min_size_mb 100
+##               --cleanup_type file --min_size_mb 200
 ##
 ##    # Remove folders: Cleanup subdirectories, keeping latest 2 directories
 ##    python cleanup_old_files.py --working_dir "/opt/app" --filename_pattern ".*" --cleanup_type directory
@@ -25,8 +25,15 @@
 ## Created : <2017-05-03>
 ## Updated: Time-stamp: <2017-05-03 16:15:17>
 ##-------------------------------------------------------------------
+import os, sys
 import argparse
 import re
+
+def remove_old_files(filename_pattern, min_copies):
+    return True
+
+def remove_old_folders(filename_pattern, min_copies, min_size_mb):
+    return True
 
 if __name__ == '__main__':
     # get parameters from users
@@ -40,14 +47,23 @@ if __name__ == '__main__':
                         help="Whether to perform the cleanup for files or directories", type=str)
     parser.add_argument('--min_copies', default=3, required=False, \
                         help='minimal shards each elasticsearch index should have', type=int)
-    parser.add_argument('--min_size_mb', default='100', required=False, \
+    parser.add_argument('--min_size_mb', default='10', required=False, \
                         help='When remove files, skip files too small', type=str)
     l = parser.parse_args()
 
     working_dir = l.working_dir
-    filename_pattern = l.filename_pattern
     cleanup_type = l.cleanup_type
+    filename_pattern = l.filename_pattern
     min_copies = l.min_copies
     min_size_mb = l.min_size_mb
 
+    if os.path.exists(working_dir) is False:
+        print "Warning: %d doesn't exists" % (working_dir)
+        sys.exit(0)
+
+    os.chdir(working_dir)
+    if cleanup_type == 'file':
+        remove_old_files(filename_pattern, min_copies)
+    else:
+        remove_old_folders(filename_pattern, min_copies, min_size_mb)
 ## File : cleanup_old_files.py ends
