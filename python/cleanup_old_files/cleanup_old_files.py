c7671a9dd2428931559877ec7ca850641228d0f1
dennyzhang <denny@dennyzhang.com>
2017-05-04 16:09:36 -0500

    list delete candidates

--- python/cleanup_old_files/cleanup_old_files.py
+++ python/cleanup_old_files/cleanup_old_files.py
@@ -14,6 +14,10 @@
 ##    python cleanup_old_files.py --working_dir "/opt/app" --filename_pattern "app-.*-SNAPSHOT.jar" \
 ##               --cleanup_type file --min_copies 3 --min_size_mb 10
 ##
+##    # Only list delete candidates, instead of perform the actual changes
+##    python cleanup_old_files.py --working_dir "/opt/app" --filename_pattern "app-.*-SNAPSHOT.jar" \
+##               --examine_only true
+##
 ##    # Remove files: Only cleanup files over 200MB
 ##    python cleanup_old_files.py --working_dir "/opt/app" --filename_pattern "app-.*-SNAPSHOT.jar" \
 ##               --cleanup_type file --min_size_mb 200
@@ -29,10 +33,14 @@ import os, sys
 import argparse
 import re
 
-def remove_old_files(filename_pattern, min_copies):
+# TODO: add logging
+
+def remove_old_files(filename_pattern, min_copies, examine_only):
+    # TODO: implement the logic
     return True
 
-def remove_old_folders(filename_pattern, min_copies, min_size_mb):
+def remove_old_folders(filename_pattern, min_copies, min_size_mb, examine_only):
+    # TODO: implement the logic
     return True
 
 if __name__ == '__main__':
@@ -40,6 +48,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--working_dir', required=True, \
                         help="Perform cleanup under which directory", type=str)
+
+    parser.add_argument('--examine_only', required=False, \
+                        help="Only list delete candidates, instead perform the actual removal", type=bool)
 
     parser.add_argument('--filename_pattern', required=False, default=".*", \
                         help="Filter files/directories by filename, before cleanup", type=str)
@@ -52,6 +63,7 @@ if __name__ == '__main__':
     l = parser.parse_args()
 
     working_dir = l.working_dir
+    examine_only = l.examine_only
     cleanup_type = l.cleanup_type
     filename_pattern = l.filename_pattern
     min_copies = l.min_copies
@@ -63,7 +75,7 @@ if __name__ == '__main__':
 
     os.chdir(working_dir)
     if cleanup_type == 'file':
-        remove_old_files(filename_pattern, min_copies)
+        remove_old_files(filename_pattern, min_copies, examine_only)
     else:
-        remove_old_folders(filename_pattern, min_copies, min_size_mb)
+        remove_old_folders(filename_pattern, min_copies, min_size_mb, examine_only)
 ## File : cleanup_old_files.py ends
