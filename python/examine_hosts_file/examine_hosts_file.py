baa746d1bb8de86e9deabef669f7ebc03eca0db8
dennyzhang <denny@dennyzhang.com>
2017-05-10 16:26:40 -0700

    implement the logic

--- python/examine_hosts_file/examine_hosts_file.py
+++ python/examine_hosts_file/examine_hosts_file.py
@@ -8,7 +8,7 @@
 ## File : examine_hosts_file.py
 ## Author : Denny <denny@dennyzhang.com>
 ## Created : <2017-05-03>
-## Updated: Time-stamp: <2017-05-10 16:07:03>
+## Updated: Time-stamp: <2017-05-10 16:26:30>
 ## Description :
 ##    Examine /etc/hosts:
 ##        1. Whether expected list of ip-hostname are included in /etc/hosts
@@ -19,8 +19,6 @@
 ##-------------------------------------------------------------------
 import os, sys
 import argparse
-import glob
-import shutil
 
 import logging
 log_file = "/var/log/%s.log" % (os.path.basename(__file__).rstrip('\.py'))
@@ -28,90 +26,35 @@ log_file = "/var/log/%s.log" % (os.path.basename(__file__).rstrip('\.py'))
 logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s %(message)s')
 logging.getLogger().addHandler(logging.StreamHandler())
 
-def list_old_files(filename_pattern, min_copies, min_size_mb):
+def load_hosts_to_list():
     l = []
-    files = glob.glob(filename_pattern)
-    files.sort(key=os.path.getmtime, reverse=True)
-    i = 0
-    for f in files:
-        if os.path.isfile(f) is False:
-            continue
-        # skip too small files
-        filesize_mb = os.stat(f).st_size/(1000*1000)
-        if filesize_mb < min_size_mb:
-            continue
-        i = i + 1
-        if i > min_copies:
-            l.append(f)
-    return l
+    with open('/etc/hosts','r') as f:
+        for row in f:
+            row = row.strip()
+            if row.startswith('#') or row == '':
+                continue
+            entry_l = row.split()
 
-def list_old_folders(filename_pattern, min_copies):
-    l = []
-    files = glob.glob(filename_pattern)
-    files.sort(key=os.path.getmtime, reverse=True)
-    i = 0
-    for f in files:
-        if os.path.isdir(f) is False:
-            continue
-        i = i + 1
-        if i > min_copies:
-            l.append(f)
+            if '::' in entry_l[0]:
+                continue
+
+            if len(entry_l) == 2:
+                l.append((entry_l[0], entry_l[1]))
+            else:
+                ip = entry_l[0]
+                for hostname in entry_l[1:]:
+                    l.append((ip, hostname))
     return l
 
 if __name__ == '__main__':
     # get parameters from users
     parser = argparse.ArgumentParser()
-    parser.add_argument('--working_dir', required=True, \
-                        help="Perform cleanup under which directory", type=str)
+    parser.add_argument('--extra_hosts_file', required=False, \
+                        help="Make sure extra hosts mapping are already in place for /etc/hosts", type=str)
 
-    parser.add_argument('--examine_only', dest='examine_only', action='store_true', default=False, \
-                        help="Only list delete candidates, instead perform the actual removal")
-
-    parser.add_argument('--filename_pattern', required=False, default=".*", \
-                        help="Filter files/directories by filename, before cleanup", type=str)
-    parser.add_argument('--cleanup_type', required=False, default='file', \
-                        help="Whether to perform the cleanup for files or directories", type=str)
-    parser.add_argument('--min_copies', default=3, required=False, \
-                        help='minimal copies to keep, before removal.', type=int)
-    parser.add_argument('--min_size_mb', default=10, required=False, \
-                        help='When remove files, skip files too small. It will be skipped when removing directories', type=int)
     l = parser.parse_args()
 
-    working_dir = l.working_dir
-    examine_only = l.examine_only
-    cleanup_type = l.cleanup_type.lower()
-    filename_pattern = l.filename_pattern
-    min_copies = l.min_copies
-    min_size_mb = l.min_size_mb
+    extra_hosts_file = l.extra_hosts_file
 
-    logging.info("Start to run cleanup for folder: %s." % working_dir)
-    if os.path.exists(working_dir) is False:
-        logging.warning("Directory(%s) doesn't exists." % (working_dir))
-        sys.exit(0)
-
-    os.chdir(working_dir)
-    if cleanup_type == 'file':
-        l = list_old_files(filename_pattern, min_copies, min_size_mb)
-    else:
-        l = list_old_folders(filename_pattern, min_copies)
-
-    if l == []:
-        logging.info("No matched files/directories to be clean.")
-        sys.exit(0)
-    else:
-        if examine_only is True:
-            logging.info("Below files/directories are selected to be removed: %s.\n"\
-                         "Skip following removal, since --examine_only has already been given." \
-                         % ",".join(l)) 
-            sys.exit(0)
-        else:
-            # Perform the actual removal
-            for f in l:
-                if cleanup_type == 'file':
-                    logging.info("Remove file. %s/%s." % (working_dir, f))
-                else:
-                    logging.info("Remove folder: %s/%s." % (working_dir, f))
-                # TODO: error handling
-                shutil.rmtree(f)
-            logging.info("Cleanup is done.")
+    print load_hosts_to_list()
 ## File : examine_hosts_file.py ends
