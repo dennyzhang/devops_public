9483e6ae244918b55959d6686068285716d70adf
dennyzhang <denny@dennyzhang.com>
2017-05-14 16:52:41 -0500

    update scripts

---
+++ python/docker_tool/refresh_containers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://raw.githubusercontent.com/DennyZhang/devops_public/master/LICENSE
+##
+## File : refresh_containers.py
+## Author : Denny <denny@dennyzhang.com>
+## Description :
+## --
+## Created : <2017-05-12>
+## Updated: Time-stamp: <2017-05-13 15:32:26>
+##-------------------------------------------------------------------
+def test():
+    print("hello, world")
+    
+if __name__ == '__main__':
+    test()
+## File : refresh_containers.py ends
