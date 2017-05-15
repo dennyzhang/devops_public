ba633c502dac85a9f562d911804635a4ed982170
dennyzhang <denny@dennyzhang.com>
2017-05-15 11:39:49 -0500

    add scripts

---
+++ python/docker_tool/backup_docker_volumes.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://raw.githubusercontent.com/DennyZhang/devops_public/master/LICENSE
+##
+## File : backup_docker_volumes.py
+## Author : Denny <denny@dennyzhang.com>
+## Description :
+## Usage:
+##            python /usr/sbin/backup_docker_volumes.py --docker_volume_list \
+##               "cijenkins_volume_backup,cijenkins_volume_jobs,cijenkins_volume_workspace" \
+##               --volume_directory "/var/lib/docker/volumes" --backup_directory "/data/backup/"
+## --
+## Created : <2017-05-12>
+## Updated: Time-stamp: <2017-05-15 11:39:38>
+##-------------------------------------------------------------------
+
+## File : backup_docker_volumes.py ends
