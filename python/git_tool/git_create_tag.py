7cd5a8907c68f8f7e44c8ff8dc2bdb3fca2fc1d4
dennyzhang <denny@dennyzhang.com>
2017-05-20 00:24:09 -0500

    update scripts

--- python/git_tool/git_create_tag.py
+++ python/git_tool/git_create_tag.py
@@ -24,37 +24,53 @@
 ## Updated: Time-stamp: <2017-05-20 00:12:12>
 ##-------------------------------------------------------------------
 import os, sys
-import sys
 import logging
 import argparse
-# Notice: Need to run: pip install GitPython
+
+import datetime
+# Install package first: pip install GitPython
 import git
 
 log_file = "/var/log/%s.log" % (os.path.basename(__file__).rstrip('\.py'))
 logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s %(message)s')
 logging.getLogger().addHandler(logging.StreamHandler())
 
-def git_create_tag(repo_url, delete_tag_already_exists):
+def git_create_tag(repo_url, tag_name, delete_tag_already_exists):
+    return True
+
+def git_list_create_tag(git_list_file, tag_name, delete_tag_already_exists):
+    git_list = []
+    with open(git_list_file,'r') as f:
+        for row in f:
+            row = row.strip()
+            if row == "" or row.startswith('#'):
+                continue
+            git_list.append(row)
+
+    for git in git_list:
+        git_create_tag(repo_url, tag_name, delete_tag_already_exists)
     return True
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--code_dirs', required=True, \
+##        python git_create_tag.py -tag_name "2017-08-01" --delete_tag_already_exists \
+##               --git_list_file /tmp/git_list.txt"
+    parser.add_argument('--tag_name', required=False, default=datetime.datetime.utcnow().strftime("%Y-%m-%d"), \
+                        help="Code directories to pull. If multiple, separated by comma", type=str)
+    parser.add_argument('--delete_tag_already_exists', dest='delete_tag_already_exists', \
+                        action='store_true', default=False, \
+                        help="Only list delete candidates, instead perform the actual removal")
+    parser.add_argument('--git_list_file', required=False, default="", \
                         help="Code directories to pull. If multiple, separated by comma", type=str)
     l = parser.parse_args()
-    code_dirs = l.code_dirs
+    tag_name = l.tag_name
+    delete_tag_already_exists = l.delete_tag_already_exists
+    git_list_file = l.git_list_file
 
-    separator = ","
-    for code_dir in code_dirs.split(separator):
-        git_output = git_pull(code_dir)
-        if git_output == 'Already up-to-date.':
-            has_changed = False
-        else:
-            has_changed = True
-            logging.info("Code has changed in %s. Detail: %s" % (code_dir, git_output))
-
-    if git_output is True:
-        sys.exit(1)
+    if git_list_create_tag(git_list_file, tag_name, delete_tag_already_exists) is True:
+        logging.info("OK: Action is done successfully.")
+        sys.exit(0)
     else:
+        logging.error("ERROR: Action has failed.")
         sys.exit(0)
 ## File : git_create_tag.py ends
