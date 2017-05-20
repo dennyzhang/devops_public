1333f5919494f650159f230e905f42ab78f7dd4a
dennyzhang <denny@dennyzhang.com>
2017-05-20 00:17:52 -0500

    update code

--- python/git_tool/git_create_tag.py
+++ python/git_tool/git_create_tag.py
@@ -10,6 +10,7 @@
 ## Description : Create monthly git tag for all repos in your projects
 ## Dependency:
 ##        pip install GitPython
+##
 ## Sample:
 ##        python git_create_tag.py -tag_name "2017-08-01" --delete_tag_already_exists \
 ##               --git_list_file /tmp/git_list.txt"
@@ -33,17 +34,9 @@ log_file = "/var/log/%s.log" % (os.path.basename(__file__).rstrip('\.py'))
 logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s %(message)s')
 logging.getLogger().addHandler(logging.StreamHandler())
 
-def git_pull(code_dir):
-    logging.info("Run git pull in %s" %(code_dir))
-    if os.path.exists(code_dir) is False:
-        logging.error("Code directory(%s): doesn't exist" % (code_dir))
-        sys.exit(1)
-    os.chdir(code_dir)
-    g = git.cmd.Git(code_dir)
-    output = g.pull()
-    return output
+def git_create_tag(repo_url, delete_tag_already_exists):
+    return True
 
-# Sample python git_pull_codedir.py --code_dirs "/data/code_dir/repo1,/data/code_dir/repo2"
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--code_dirs', required=True, \
