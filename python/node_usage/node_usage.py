3f9fd35797d2efd6d85813e139a9ccc7ad076a1c
dennyzhang <denny@dennyzhang.com>
2017-05-22 22:48:44 -0500

    show node status

--- python/node_usage/node_usage.py
+++ python/node_usage/node_usage.py
@@ -9,13 +9,15 @@
 ## Description : Show OS and process resource usage: CPU, RAM and disk
 ## --
 ## Created : <2017-05-22>
-## Updated: Time-stamp: <2017-05-22 22:34:32>
+## Updated: Time-stamp: <2017-05-22 22:43:07>
 ##-------------------------------------------------------------------
 import psutil
 
 def get_os_usage():
-    cpu_percent = psutil.cpu_percent()
     memory_usage = psutil.virtual_memory()
+    cpu_percent = psutil.cpu_percent()
+    # TODO: How many cpu cores
+    # TODO: disk usage
     return (cpu_percent, memory_usage)
 
 def get_process_usage(pid):
@@ -28,9 +30,10 @@ def show_usage():
     memory_total_mb = memory_usage.total/(1024*1024)
     memory_available_mb = memory_usage.available/(1024*1024)
     memory_buffers_mb = memory_usage.buffers/(1024*1024)
-    print("CPU percent: %f." % (cpu_percent))
-    print("Memory Summary. Available: %f, buffered: %f, total: %f" % \
-          (memory_total_mb, memory_available_mb, memory_buffers_mb))
+    print("CPU Percent: %s" % ("{:.2f}".format(cpu_percent*100))+"%")
+    print("Memory Summary. Available: %sMB, buffered: %sMB, total: %sMB" % \
+          ("{:.2f}".format(memory_total_mb), "{:.2f}".format(memory_available_mb), \
+           "{:.2f}".format(memory_buffers_mb)))
 
 if __name__ == '__main__':
     show_usage()
