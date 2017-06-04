2264380f8de97bc4041263cf3a1203bee5527081
dennyzhang <denny@dennyzhang.com>
2017-06-04 16:27:41 -0500

    shellcheck fix

--- python/git_tool/git_pull_codedir.sh
+++ python/git_tool/git_pull_codedir.sh
@@ -10,7 +10,7 @@
 ## Sample: bash git_pull_codedir.sh "/data/code_dir/repo1,/data/code_dir/repo2"
 ## --
 ## Created : <2017-06-04>
-## Updated: Time-stamp: <2017-06-04 16:26:33>
+## Updated: Time-stamp: <2017-06-04 16:27:31>
 ##-------------------------------------------------------------------
 # https://coderwall.com/p/rdi_wq/fix-could-not-open-a-connection-to-your-authentication-agent-when-using-ssh-add
 
@@ -23,16 +23,16 @@ function detect_ssh_auth_sock() {
 function get_agentid_from_authsock() {
     # Sample: /tmp/ssh-CQvj4eoYn5ha/agent.2340 -> 2340
     ssh_auth_sock=${1?}
-    echo ${ssh_auth_sock##*.}
+    echo "${ssh_auth_sock##*.}"
 }
 
 code_dir_list=${1?}
 ssh_auth_sock=$(detect_ssh_auth_sock)
 ssh_agent_id=$(get_agentid_from_authsock "ssh_auth_sock")
-export SSH_AUTH_SOCK="$ssh_auth"
+export SSH_AUTH_SOCK="$ssh_auth_sock"
 export SSH_AGENT_PID="$ssh_agent_id"
 
-echo "export SSH_AUTH_SOCK=$ssh_auth; export SSH_AGENT_PID=$ssh_agent_id"
+echo "export SSH_AUTH_SOCK=$ssh_auth_sock; export SSH_AGENT_PID=$ssh_agent_id"
 if [ ! -f /usr/sbin/git_pull_codedir.py ]; then
     echo "ERROR: /usr/sbin/git_pull_codedir.py is not found!"
     echo "Get it from: https://github.com/DennyZhang/devops_public/tree/master/python/git_tool"
