13a724b140e39486460f313a8c79ff9c2f10d4ad
dennyzhang <denny@dennyzhang.com>
2017-06-07 21:24:59 -0500

    run gui test

--- python/selenium_load_page/selenium_gui_test.sh
+++ python/selenium_load_page/selenium_gui_test.sh
@@ -5,15 +5,18 @@
 ## Description :
 ## --
 ## Created : <2017-05-18>
-## Updated: Time-stamp: <2017-06-07 21:17:59>
+## Updated: Time-stamp: <2017-06-07 21:23:10>
 ##-------------------------------------------------------------------
 url_test=${1?}
 test_py_script=${2?"/home/seluser/scripts/selenium_load_page.py"}
 maximum_seconds=${3:-"30"}
 container_name=${4:-"selenium"}
-bind_hosts_list=${5:-""}
+add_host=${5:-""}
 
+image_name="denny/selenium:v1"
+py_script_in_container="/home/seluser/scripts/selenium_load_page.py"
 working_dir="/root/selenium_test"
+
 [ -d "$working_dir" ] || mkdir -p "$working_dir"
 
 function destroy_container() {
@@ -35,14 +38,15 @@ trap shell_exit SIGHUP SIGINT SIGTERM 0
 # Start docker container
 destroy_container "$container_name"
 
-image_name="denny/selenium:v1"
-py_script_in_container="/home/seluser/scripts/selenium_load_page.py"
-docker run -t -d --privileged -h selenium \
-       -v "$test_py_script:$py_script_in_container" \
-       --name "$container_name" "$image_name"
-
-if [ -n "$bind_hosts_list" ]; then
-    echo "TODO: update hosts"
+if [ -z "$add_host" ]; then
+    docker run -t -d --privileged -h selenium \
+           -v "$test_py_script:$py_script_in_container" \
+           --name "$container_name" "$image_name"
+else
+    docker run -t -d --privileged -h selenium \
+           --add-host="$add_host" \
+           -v "$test_py_script:$py_script_in_container" \
+           --name "$container_name" "$image_name"
 fi
 
 # TODO: better way
