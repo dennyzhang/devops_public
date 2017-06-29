f8901eb2546813d58fc2816d00aed109bd60654a
dennyzhang <denny@dennyzhang.com>
2017-06-29 14:33:32 -0500

    minor change

---
+++ python/enforce_rubocopcheck/docker_rubocopcheck.sh
@@ -0,0 +1,67 @@
+#!/bin/bash -e
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT 
+##   https://raw.githubusercontent.com/DennyZhang/devops_public/master/LICENSE
+##
+## File : docker_rubocopcheck.sh
+## Author : Denny <denny@dennyzhang.com>
+## Description :
+##
+## More reading: TODO
+##
+## --
+## Created : <2017-05-12>
+## Updated: Time-stamp: <2017-06-29 14:32:57>
+##-------------------------------------------------------------------
+code_dir=${1?""}
+ignore_file_list=${2-""}
+exclude_code_list=${3-""}
+
+image_name="denny/rubocop:1.0"
+check_filename="/enforce_rubocopcheck.py"
+
+current_filename=$(basename "$0")
+test_id="${current_filename%.sh}_$$"
+container_name="$test_id"
+ignore_file="$test_id"
+
+function remove_container() {
+    container_name=${1?}
+    if docker ps -a | grep "$container_name" 1>/dev/null 2>&1; then
+        echo "Destroy container: $container_name"
+        docker stop "$container_name"; docker rm "$container_name"
+    fi
+}
+
+function shell_exit() {
+    errcode=$?
+    if [ $errcode -eq 0 ]; then
+        echo "Test has passed."
+    else
+        echo "ERROR: Test has failed."
+    fi
+
+    echo "Remove tmp file: $ignore_file"
+    rm -rf "/tmp/$ignore_file"
+
+    remove_container "$container_name"
+    exit $errcode
+}
+
+################################################################################
+trap shell_exit SIGHUP SIGINT SIGTERM 0
+
+echo "Generate the ignore file for code check"
+echo "$ignore_file_list" > "/tmp/$ignore_file"
+
+echo "Start container"
+remove_container "$container_name"
+docker run -t -d --privileged -v "${code_dir}:/code" --name "$container_name" --entrypoint=/bin/sh "$image_name"
+
+echo "Copy ignore file"
+docker cp "/tmp/$ignore_file" "$container_name:/$ignore_file"
+
+echo "Run code check: python $check_filename --code_dir /code --check_ignore_file /${ignore_file} --exclude_code_list ${exclude_code_list}"
+docker exec -t "$container_name" python "$check_filename" --code_dir /code --check_ignore_file "/${ignore_file}" --exclude_code_list "${exclude_code_list}"
+## File : docker_rubocopcheck.sh ends
---
+++ python/enforce_rubocopcheck/enforce_rubocopcheck.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://raw.githubusercontent.com/DennyZhang/devops_public/master/LICENSE
+##
+## File : enforce_rubocopcheck.py
+## Author : Denny <denny@dennyzhang.com>
+## Description :
+##
+## More reading: TODO
+##
+## --
+## Created : <2017-04-02>
+## Updated: Time-stamp: <2017-06-29 14:33:20>
+##-------------------------------------------------------------------
+import argparse
+import sys
+import os
+import subprocess
+import re
+
+def find_files_by_postfix(folder_check, filename_postfix):
+    l = []
+    for root, dirs, files in os.walk(folder_check):
+        for file in files:
+            if file.endswith(filename_postfix):
+                l.append(os.path.join(root, file))
+    return l
+
+def ignore_files(file_list, ignore_file_list):
+    if ignore_file_list is None:
+        return file_list
+
+    l = []
+    for fname in file_list:
+        skip = False
+        for ignore_file_pattern in ignore_file_list:
+            ignore_file_pattern = ignore_file_pattern.strip().strip("\n")
+            if ignore_file_pattern == "":
+                continue
+            if re.search(ignore_file_pattern, fname) is not None :
+                skip = True
+                break
+        if skip is False:
+            l.append(fname)
+    return l
+
+def run_check(file_list, check_pattern):
+    has_error = False
+    for fname in file_list:
+        check_command = check_pattern % (fname)
+        print "Run check command: %s" % (check_command)
+        returncode = subprocess.call(check_command, shell=True)
+        if returncode != 0:
+            has_error = True
+            print "Error to run %s. Return code: %d" % (check_command, returncode)
+    return has_error
+################################################################################
+#
+# wget -O /tmp/enforce_rubocopcheck.py https://raw.githubusercontent.com/DennyZhang/devops_public/tag_v6/python/enforce_rubocopcheck/enforce_rubocopcheck.py
+# python /tmp/enforce_rubocopcheck.py --code_dir devops_code/devops_public
+################################################################################
+
+if __name__ == '__main__':
+    # get parameters from users
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--code_dir', required=False, default=".", \
+                        help="Source code directory to be scanned", type=str)
+    parser.add_argument('--check_ignore_file', required=False, \
+                        help="file pattern listed in the file will be skipped for scan", type=str)
+    parser.add_argument('--exclude_code_list', required=False, \
+                        default="SC1090,SC1091,SC2154,SC2001,SC2002,SC2181", \
+                        help="rubocopcheck code to be skipped", type=str)
+
+    l = parser.parse_args()
+
+    code_dir = os.path.expanduser(l.code_dir)
+    check_ignore_file = l.check_ignore_file
+    if check_ignore_file != "":
+        check_ignore_file = os.path.expanduser(l.check_ignore_file)
+
+    exclude_code_list = l.exclude_code_list
+
+    print "Run rubocopcheck for *.sh under %s" % (code_dir)
+    file_list = find_files_by_postfix(code_dir, ".sh")
+    if check_ignore_file != "":
+        with open(check_ignore_file) as f:
+            ignore_file_list = f.readlines()
+            file_list = ignore_files(file_list, ignore_file_list)
+
+    has_error = run_check(file_list, \
+                          "rubocopcheck -e " + exclude_code_list + " %s")
+    if has_error is False:
+        print "OK: no error detected from rubocopcheck"
+        sys.exit(0)
+    else:
+        print "ERROR: %s has failed." % (os.path.basename(__file__))
+        sys.exit(1)
+## File : enforce_rubocopcheck.py ends
