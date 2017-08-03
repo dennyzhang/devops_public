91449465489ccc71e4d0b5527f0b4b54526b3c02
dennyzhang <denny@dennyzhang.com>
2017-08-03 18:12:30 -0500

    update comment

--- python/parameters_tool/strip_comments.py
+++ python/parameters_tool/strip_comments.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 ## File : strip_comments.py
 ## Created : <2017-08-03>
-## Updated: Time-stamp: <2017-08-03 18:09:41>
+## Updated: Time-stamp: <2017-08-03 18:12:22>
 ## Description :
 ##    For a block of string, remove useless stuff
 ##       1. Remove leading whitespace
@@ -9,7 +9,15 @@
 ##       3. Remove any lines start with #
 ##
 ## Sample:
+##    export server_list="# server ip
+##             
+##             ## APP
+##             138.68.52.73:22
+##             ## loadbalancer
+##             #138.68.254.56:2711
+##             #138.68.254.215:2712"
 ##    server_list=$(echo "$server_list" | python ./strip_comments.py)
+##    server_list: "138.68.52.73:22"
 ##-------------------------------------------------------------------
 import os, sys
 
